717e4b2b1a65404502c9b86db2b1bfb7b006b5fb
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-02-28 16:19:52 -0300

    Actual Topic: Install and Setup | Next Topic: Break it down

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add -A
-git commit -m "Project started | Files included: README.md / guide_content.md / autogit.sh"
-git branch -M master
-git remote add origin https://github.com/CarlosViniMSouza/FastAPI_FullCourse.git
-git push -u origin master
+git commit -m "Actual Topic: Install and Setup | Next Topic: Break it down"
+git push
 git status
